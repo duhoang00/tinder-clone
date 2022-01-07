9811d9c00eb23cca1bbb6bdd9d7d3f94a6107571
Du Hoang <hoangminhdu2000@gmail.com>
2022-01-08 06:44:38 +0700

    update dashboard

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,16 +1,66 @@
-import { Card } from "antd";
+import { useState } from "react";
+import { Card, Row, Col, Carousel, Radio, Button, Tooltip } from "antd";
+import { HeartFilled, CloseOutlined } from "@ant-design/icons";
+
+import styles from "./Dashboard.module.less";
 
 const Dashboard = () => {
   return (
-    <Card
-      title="Default size card"
-      extra={<a href="#">More</a>}
-      style={{ width: 300 }}
-    >
-      <p>Card content</p>
-      <p>Card content</p>
-      <p>Card content</p>
-    </Card>
+    <div className={styles.dashboard}>
+      <Row>
+        <Col>
+          <Card>
+            <Carousel dotPosition={"top"}>
+              <div>
+                <h3>1</h3>
+              </div>
+              <div>
+                <h3>2</h3>
+              </div>
+              <div>
+                <h3>3</h3>
+              </div>
+              <div>
+                <h3>4</h3>
+              </div>
+            </Carousel>
+          </Card>
+        </Col>
+      </Row>
+      <Row justify="center" align="bottom">
+        <Col>
+          <Tooltip title="Pass">
+            <Button
+              shape="circle"
+              icon={
+                <CloseOutlined
+                  style={{
+                    color: "#f86871",
+                    strokeWidth: "100",
+                    stroke: "#f86871",
+                  }}
+                />
+              }
+              type="text"
+              danger
+            ></Button>
+          </Tooltip>
+          <Tooltip title="Like">
+            <Button
+              shape="circle"
+              icon={
+                <HeartFilled
+                  style={{
+                    color: "#3eebc3",
+                    stroke: "#3eebc3",
+                  }}
+                />
+              }
+            ></Button>
+          </Tooltip>
+        </Col>
+      </Row>
+    </div>
   );
 };
 
